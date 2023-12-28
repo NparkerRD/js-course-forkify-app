8abd2faa57cdc816713dba0f584bba4ede10f710
unknown <nicole@rocketdaddy.ai>
2023-12-28 13:47:49 -0500

    lesson 303-308.15

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,8 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'regenerator-runtime/runtime'; // Poly-filling async await
 import 'core-js/stable'; // Poly-filling everything else
@@ -27,7 +29,6 @@ const timeout = function (s) {
 const controlRecipes = async function() {
     try {
         const id = window.location.hash.slice(1);
-        console.log(id);
 
         if (!id) return;
         recipeView.renderSpinner();
@@ -35,14 +36,19 @@ const controlRecipes = async function() {
         // 0) Update results view to mark selected search result
         resultsView.update(model.getSearchResultsPage());
 
-        // 1) Loading recipe
+        // 1) Updating bookmarks view
+        bookmarksView.update(model.state.bookmarks);
+        
+        // 2) Loading recipe
         await model.loadRecipe(id);
         
-        // 2) Rendering recipe
+        // 3) Rendering recipe
         recipeView.render(model.state.recipe);
+
 
     } catch (err) {
         recipeView.renderError();
+        console.error(err);
     }
 }
 
@@ -87,12 +93,36 @@ const controlServings = function (newServings) {
     recipeView.update(model.state.recipe);
 }
 
+const controlAddBookmark = function () {
+    // 1) Add/remove bookmark
+    if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+    else model.deleteBookmark(model.state.recipe.id);
+    
+    // 2) Update recipe view
+    recipeView.update(model.state.recipe);
+
+    // 3) Render bookmarks
+    bookmarksView.render(model.state.bookmarks);
+}
+
+const controlBookmarks = function () {
+    bookmarksView.render(model.state.bookmarks);
+}
+
+const controlAddRecipe = function(newRecipe) {
+    console.log(newRecipe);
+
+    //  Upload the new recipe data
+}
+
 const init = function () {
+    bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
     recipeView.addHandlerUpdateServings(controlServings);
+    recipeView.addHandlerAddBookmark(controlAddBookmark); // BOOKMARK
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
-    
+    addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg' // Parcel 2
 import {Fraction} from 'fractional';
-console.log(Fraction);
+
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -21,6 +21,17 @@ class RecipeView extends View {
         if(+updateTo > 0) handler(+updateTo);
       })
     }
+
+
+    addHandlerAddBookmark(handler){
+      this._parentElement.addEventListener('click', (e) => {
+        const btn = e.target.closest('.btn--bookmark');
+        if (!btn) return;
+        
+        handler();
+      })
+    }
+    
 
     _generateMarkup() {
         // console.log(this._data);
@@ -63,9 +74,9 @@ class RecipeView extends View {
 
           <div class="recipe__user-generated">
           </div>
-          <button class="btn--round">
+          <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : ''}"></use>
             </svg>
           </button>
         </div>
